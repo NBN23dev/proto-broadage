576de1a903f77a37d0a8bc3ab3207c9594aa3282
Vicente Gavara <vgavara@gmail.com>
2024-04-03 17:25:58 +0200

    Schema bugs fixed

--- pkg/api/v1/proto_broadage.pb.gw.go
+++ pkg/api/v1/proto_broadage.pb.gw.go
@@ -153,6 +153,10 @@ func local_request_BroadageService_GetMatchPlayers_0(ctx context.Context, marsha
 
 }
 
+var (
+	filter_BroadageService_GetPlayByPlay_0 = &utilities.DoubleArray{Encoding: map[string]int{"match_fid": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
+)
+
 func request_BroadageService_GetPlayByPlay_0(ctx context.Context, marshaler runtime.Marshaler, client BroadageServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq GetPlayByPlayRequest
 	var metadata runtime.ServerMetadata
@@ -172,6 +176,13 @@ func request_BroadageService_GetPlayByPlay_0(ctx context.Context, marshaler runt
 	protoReq.MatchFid, err = runtime.String(val)
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "match_fid", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_BroadageService_GetPlayByPlay_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := client.GetPlayByPlay(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
@@ -198,6 +209,13 @@ func local_request_BroadageService_GetPlayByPlay_0(ctx context.Context, marshale
 	protoReq.MatchFid, err = runtime.String(val)
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "match_fid", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_BroadageService_GetPlayByPlay_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := server.GetPlayByPlay(ctx, &protoReq)
